8c5012c7588c08af1b30f8ced20ea4c710ebb318
0meeee0 <mehdi74.id@gmail.com>
2024-12-20 11:22:25 +0100

    Feat: data of the books is being retrieved

--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -12,7 +12,7 @@ export default function BookList() {
             console.log(res.data.member)
         }catch(err){
             console.error(err)
-        }
+        } 
     }
     fetchBooks()
   },[])
